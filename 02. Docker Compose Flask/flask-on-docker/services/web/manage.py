2d914030e364e41242fd9b323a0740a0b6df6f25
abdulaziz zaid <aziz.sa03@gmail.com>
2024-07-03 23:24:22 +0300

    3

--- 02. Docker Compose Flask/flask-on-docker/services/web/manage.py
+++ 02. Docker Compose Flask/flask-on-docker/services/web/manage.py
@@ -1,8 +1,14 @@
 from flask.cli import FlaskGroup
+from project import server, db
 
-from project import server
 
 cli = FlaskGroup(server)
 
+@cli.command("create_db")
+def create_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+    
 if __name__ == "__main__":
     cli()
--- 02. Docker Compose Flask/flask-on-docker/services/web/project/__init__.py
+++ 02. Docker Compose Flask/flask-on-docker/services/web/project/__init__.py
@@ -1,6 +1,14 @@
-from flask import Flask # ignore the error, it's a bug in the linter
-
+from flask import Flask # type: ignore # ignore the error, it's a bug in the linter
+from flask_sqlalchemy import SQLAlchemy # type: ignore # ignore the error, it's a bug in the linter
 server = Flask(__name__)
+server.config.from_object('project.config.Config')
+db = SQLAlchemy(server)
+
+class User(db.Model):
+    __table_name__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(128), nullable=False, unique=True)
+    
 @server.route('/')
 def hello():
     return 'Hello Wdddorld!'
