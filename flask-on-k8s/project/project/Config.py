fbeeab7f4d3c13ff071a14e60e1f006bf694db46
aziz <aziz.sa03@gmail.com>
2024-07-04 16:21:53 +0300

    Fix getEnv for databases

--- flask-on-k8s/project/project/Config.py
+++ flask-on-k8s/project/project/Config.py
@@ -1,4 +1,4 @@
 import os
 
 class Config():
-    SQLALCHEMY_DATABASE_URI = "postgresql://" + os.environ['POSTGRES_USER'] + ":" + os.environ['POSTGRES_PASSWORD'] + "@" + os.environ['POSTGRES_HOST'] + ":" + os.environ['POSTGRES_PORT'] + "/" + os.environ['POSTGRES_DB']
+    SQLALCHEMY_DATABASE_URI = "postgresql://" + os.getenv("POSTGRES_USER") + ":" + os.getenv("POSTGRES_PASSWORD") + "@" + os.getenv("POSTGRES_HOST") + ":" + os.getenv("POSTGRES_PORT") + "/" + os.getenv("POSTGRES_DB")
